daac4c56e1e99e92722bec5946b065a44c23552a
JEI._Grave <jei.grave@gmail.com>
2024-04-18 02:56:16 +0800

    Grail & Run V0.00

---
+++ GrainNRuin/src/main/java/com/mycompany/grainnruin/GrainNRuin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grainnruin;
+
+/**
+ *
+ * @author Engilo Grave
+ */
+public class GrainNRuin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
