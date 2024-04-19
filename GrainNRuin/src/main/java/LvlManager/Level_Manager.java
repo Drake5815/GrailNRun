6974edcd03762a6be39a8c01466f12ec9965d99c
JEI._Grave <jei.grave@gmail.com>
2024-04-19 14:11:02 +0800

    Including lvlManager
    Implementing lvlManager in Project

---
+++ GrainNRuin/src/main/java/LvlManager/Level_Manager.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LvlManager;
+
+/**
+ *
+ * @author Engilo Grave
+ */
+public class Level_Manager {
+    //Names
+    String Username = "";
+    String AvatarName = "";
+    
+    //UI_Controller
+    
+    //Relic_Manager
+    
+    //Crad_Manager
+    
+}
